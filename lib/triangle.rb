d09341a24a79fb9b3021930d5f5a033ce98a0d11
Sheriefs Gamble <sgamble284@gmail.com>
2020-01-07 16:46:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,10 +13,7 @@ class Triangle
       raise TriangleError
       elsif (@side_1 + @side_2 <= @side_3) || (@side_2 + @side_3 <= @side_1)  || (@side_1 + @side_3 <= @side_2)
       raise TriangleError
-    if @side_1 <= 0 || @side_2 <= 0 || @side_3 <= 0
-      TriangleError
-      elsif (@side_1 + @side_2 <= @side_3) || (@side_2 + @side_3 <= @side_1)  || (@side_1 + @side_3 <= @side_2)
-      TriangleError
+    
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
         :equilateral
